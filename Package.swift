a99845993ec1e3bde5651e1594604f26c11408e6
Eric L. Hernes <eric@qinc.tv>
2024-04-26 17:32:57 -0500

    add platforms

--- Package.swift
+++ Package.swift
@@ -4,24 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "FCPXMLCodable",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "FCPXMLCodable",
-            targets: ["FCPXMLCodable"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.8.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "FCPXMLCodable",
-            dependencies: ["XMLCoder"]),
-        .testTarget(
-            name: "FCPXMLCodableTests",
-            dependencies: ["FCPXMLCodable"]),
-    ]
-)
+  name: "FCPXMLCodable",
+  platforms: [.iOS("13.0"), .macOS("16.4"],
+              products: [
+                // Products define the executables and libraries produced by a package, and make them visible to other packages.
+                .library(
+                  name: "FCPXMLCodable",
+                  targets: ["FCPXMLCodable"]),
+              ],
+              dependencies: [
+                .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.8.0")
+              ],
+              targets: [
+                // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+                // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+                .target(
+                  name: "FCPXMLCodable",
+                  dependencies: ["XMLCoder"]),
+                .testTarget(
+                  name: "FCPXMLCodableTests",
+                  dependencies: ["FCPXMLCodable"]),
+              ]
+  )
